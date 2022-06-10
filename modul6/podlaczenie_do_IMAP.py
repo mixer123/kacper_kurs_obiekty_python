f2452eaf2243f33917b5ec4bb84b917af61561f6
Mirek <miroslaw.butajlo@gmail.com>
2022-06-10 14:25:50 +0200

    v26

--- modul6/podlaczenie_do_IMAP.py
+++ modul6/podlaczenie_do_IMAP.py
@@ -8,9 +8,25 @@ password = 'JkisFQRz9VyWc2$'
 imap_server = imaplib.IMAP4_SSL(host)
 imap_server.login(user, password)
 imap_server.select('INBOX')
+# print('typ imap server select', (imap_server.select('INBOX')))
 message_ids= imap_server.search(None, 'ALL')
+# print('typ', type(message_ids[1]))
 typ, data = message_ids
+
+print('type data', type(data))
 for m in data[0].split():
     typ, data = imap_server.fetch(m, '(RFC822)')
-    print(m, data[0][1])
+    message = email.message_from_bytes(data[0][1])
+    subject, subject_encoding = decode_header(message['Subject'])[0]
+    if subject_encoding is not None:
+        subject = subject.decode('utf-8')
+    email_from , from_encoding = decode_header(message['From'])[0]
+    if from_encoding is not None:
+        email_from = email_from.decode('utf-8')
+    print(subject, email_from)
     quit()
+    # Pobieranie załaczników
+    for chunk in message.walk():
+        print(chunk.get_filename())
+        print(chunk.get_payload(decode=True))
+        quit()
